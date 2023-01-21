68d74fa8b729fe0d2e1f43a0e4cd5d15864b6b69
caiopeternela <caiopeternela.dev@gmail.com>
2023-01-21 10:05:25 -0300

    openmeteo supremacy

--- neoclima/main.py
+++ neoclima/main.py
@@ -1,13 +1,10 @@
 import typer
 import requests
 from tinydb import TinyDB, Query
-from neoclima.commands_svc import get_emoji
 
 app = typer.Typer()
 
-db = TinyDB("db.json")
-
-API_KEY = "5dae4089f7baf07089814ee51012a645"
+db = TinyDB("./db.json")
 
 @app.command()
 def now(city_nickname: str):
@@ -18,12 +15,9 @@ def now(city_nickname: str):
     if city:
         lat = city[0]["coordinates"][0]
         lon = city[0]["coordinates"][1]
-        r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&units=metric&lon={lon}&limit=1&appid={API_KEY}")
-        content = r.json()
-        current_temperature = int(content["main"]["temp"])
-        weather = content["weather"][0]["main"]
-        emoji = get_emoji(weather)
-        print(emoji + "  " + str(current_temperature) + "°C")
+        r = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=True")
+        current_temperature = int(r.json()["current_weather"]["temperature"])
+        print(str(current_temperature) + "°C")
     else:
         print("City not added!")
 
@@ -36,11 +30,10 @@ def add(city_nickname: str):
     city_nickname_exists = bool(db.search(Query().city == city_nickname))
     if not city_nickname_exists:
         country = input("Type the country name or code (ISO3166): ")
-        state = input("Type the state code (only for US): ")
         city = input("Type the city name: ")
-        r = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={API_KEY}")
-        lat = r.json()[0]["lat"]
-        lon = r.json()[0]["lon"]
+        r = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?country_code={country}&name={city}&count=1")
+        lat = r.json()["results"][0]["latitude"]
+        lon = r.json()["results"][0]["longitude"]
         db.insert({"city": city_nickname, "coordinates": [lat,lon]})
         print("City added succesfully!")
     else:
@@ -52,10 +45,10 @@ def edit(city_nickname: str):
     """
     Edit city nickname
     """
-    city_nickname_db = db.search(Query().city == city_nickname)
-    if city_nickname_db:
+    city = db.search(Query().city == city_nickname)
+    if city:
         new_city_nickname = input(f'Type the new city nickname for "{city_nickname}": ')
-        db.update({"city": new_city_nickname}, Query().city == city_nickname_db[0]["city"])
+        db.update({"city": new_city_nickname}, Query().city == city[0]["city"])
         print("City nickname updated succesfully!")
     else:
         print("City not added!")
@@ -79,7 +72,7 @@ def ls():
     """
     List added cities
     """
-    cities_nicknames = [dict["city"] for dict in db]
+    cities_nicknames = [d["city"] for d in db]
     if cities_nicknames:
         print(*cities_nicknames, sep="\n")
     else:
