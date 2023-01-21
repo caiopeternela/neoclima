c6729899dc2b62a8ac75b9bbe9411570633d5918
caiopeternela <caiopeternela.dev@gmail.com>
2023-01-21 10:21:04 -0300

    add test for the add function

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,11 @@ runner = CliRunner()
 def test_ls_command():
     result = runner.invoke(app, "ls")
     assert result.exit_code == 0
+
+
+def test_add_command():
+    result = runner.invoke(app, ["add", "nyc"], input="US\nNew York")
+    outputs = [output.strip() for output in result.stdout.split(":")]
+    assert outputs[0] == "Type the country name or code (ISO3166)"
+    assert outputs[1] == "Type the city name"
+    assert outputs[2] == "City added succesfully!"
